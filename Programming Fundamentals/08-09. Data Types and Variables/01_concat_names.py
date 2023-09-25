41bbfab78e28997e1f5c9c7104aa09fdfe1de252
Konstantin Kirev <konstantin.kirev@gmail.com>
2023-09-25 19:10:33 +0300

    just added the first two tasks

---
+++ Programming Fundamentals/08-09. Data Types and Variables/01_concat_names.py
@@ -0,0 +1,9 @@
+"""
+Write a program that reads two names and a delimiter. It should print the names joined by the delimiter.
+"""
+
+name1 = input()
+name2 = input()
+delimiter = input()
+
+print(name1 + delimiter + name2)
